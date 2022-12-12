b8a77fb31ae078f46932d4e85f570106c9b19573
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-12 20:00:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
 
+float max (float pt[], int n){
+  int i=0;
+  while (i<n){
+    if (pt[0]>pt[1]){
+      i++;
+    }
+    else {
+      pt[0]=pt[i];
+      i++;
+    }
+  }
+  return pt[0];
+}
