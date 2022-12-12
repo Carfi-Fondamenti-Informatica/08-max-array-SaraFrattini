a1f12b1f9dfa3b67e9d0e59a66aa233586cd98da
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-12 20:01:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main () {
+   int n=0;
+   cin>>n;
+   float ar[n];
+   for (int i=0; i<n; i++){
+       cin>>ar[i];
+   }
+ cout<< max (ar , n)<<endl;
+    return 0;
 }
