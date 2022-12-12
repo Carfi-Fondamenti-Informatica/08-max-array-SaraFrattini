5488393c2516e0027b4af6907d70b01060449792
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-12-12 20:09:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main () {
-   int n=0;
-   cin>>n;
-   float ar[n];
-   for (int i=0; i<n; i++){
-       cin>>ar[i];
-   }
- cout<< max (ar , n)<<endl;
+int main() {
+int n;
+cin>>n;
+float a[n];
+float x;
+for(int i=0; i<n; i++){
+    cin>>x;
+    a[i]=x;
+}
+cout<<valmax(a, n);
+cout<<valmax(a,n);
     return 0;
 }
